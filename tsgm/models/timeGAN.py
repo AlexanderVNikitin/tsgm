ab2c48b43f8a64c41089d16ad5e9b1cab6a362de
letiziaia <letizia852@gmail.com>
2022-08-21 21:46:44 +0300

    train steps skeleton

--- tsgm/models/timeGAN.py
+++ tsgm/models/timeGAN.py
@@ -22,3 +22,41 @@ class TimeGAN(keras.Model):
     """
     def __init__(self):
         pass
+
+    @tf.function
+    def _train_autoencoder(self, X, optimizer: keras.optimizers.Optimizer):
+        """
+        minimize E_loss0
+        """
+        with tf.GradientTape() as tape:
+            X_tilde = self.autoencoder(X)
+            E_loss_T0 = keras.losses.MeanSquaredError(X, X_tilde)
+            E_loss0 = 10 * tf.sqrt(E_loss_T0)
+
+        e_vars = self.embedder.trainable_variables
+        r_vars = self.recovery.trainable_variables
+        all_trainable = e_vars + r_vars
+
+        gradients = tape.gradient(E_loss0, all_trainable)
+        optimizer.apply_gradients(zip(gradients, all_trainable))
+        return E_loss0  # or tf.sqrt(E_loss_T0), it should be the same
+
+    @tf.function
+    def _train_supervisor(self, X, optimizer: keras.optimizers.Optimizer):
+        with tf.GradientTape() as tape:
+            pass
+
+    @tf.function
+    def _train_embedder(self, X, optimizer: keras.optimizers.Optimizer):
+        with tf.GradientTape() as tape:
+            pass
+
+    @tf.function
+    def _train_generator(self, X, Z, optimizer: keras.optimizers.Optimizer):
+        with tf.GradientTape() as tape:
+            pass
+
+    @tf.function
+    def _train_discrimiator(self, X, Z, optimizer: keras.optimizers.Optimizer):
+        with tf.GradientTape() as tape:
+            pass
