2a92e572547d299d9fd172c331f5bae9494cf6f5
letiziaia <letizia852@gmail.com>
2023-01-18 19:14:26 +0200

    refactor, add sliceAndShuffle

--- tsgm/models/augmentations.py
+++ tsgm/models/augmentations.py
@@ -11,16 +11,37 @@ logger.setLevel(logging.DEBUG)
 class BaseAugmenter:
     def __init__(
         self,
+        per_feature: bool,
     ):
+        self.per_channel = per_feature
+        self._data = None
+        self._targets = None
+        return
+
+    def _get_seeds(self, n: int) -> TensorLike:
+        seeds_idx = np.random.choice(
+            range(self._data.shape[0]), size=n, replace=True
+        )
+        return  seeds_idx
+
+    def _check_fitted(self):
+        if self._data is None:
+            raise AttributeError(
+                "This object is not fitted. Call .fit(your_dataset) first."
+            )
 
     def fit(self, time_series: TensorLike, y: Optional[TensorLike]):
-        raise NotImplementedError
+        self._data = time_series
+        if y is not None:
+            self._targets = y
+        return self
 
     def generate(self, n_samples: int) -> TensorLike:
         raise NotImplementedError
 
     def fit_generate(self, time_series: TensorLike, y: Optional[TensorLike], n_samples: int) -> TensorLike:
-        raise NotImplementedError
+        self.fit(time_series=time_series, y=y)
+        return self.generate(n_samples=n_samples)
 
 
 class BaseCompose:
@@ -59,7 +80,7 @@ class GaussianNoise(BaseAugmenter):
         variance: TensorLike = (10.0, 50.0),
         per_feature: bool = True,
     ):
-        super(GaussianNoise, self).__init__()
+        super(GaussianNoise, self).__init__(per_feature)
         if isinstance(variance, (tuple, list)):
             if variance[0] < 0:
                 raise ValueError("Lower var_limit should be non negative.")
@@ -76,24 +97,10 @@ class GaussianNoise(BaseAugmenter):
             )
 
         self.mean = mean
-        self.per_channel = per_feature
-        self._data = None
-        self._targets = None
-
-    def fit(self, time_series: TensorLike, y: Optional[TensorLike]) -> BaseAugmenter:
-        self._data = time_series
-        if y is not None:
-            self._targets = y
-        return self
 
     def generate(self, n_samples: int) -> TensorLike:
-        if self._data is None:
-            raise AttributeError(
-                "This object is not fitted. Call .fit(your_dataset) first."
-            )
-        seeds_idx = np.random.choice(
-            range(self._data.shape[0]), size=n_samples, replace=True
-        )
+        self._check_fitted()
+        seeds_idx = self._get_seeds(n_samples)
 
         synthetic_data = []
         labels = []
@@ -114,6 +121,51 @@ class GaussianNoise(BaseAugmenter):
         else:
             return np.array(synthetic_data)
 
-    def fit_generate(self, time_series: TensorLike, y: Optional[TensorLike], n_samples: int) -> TensorLike:
-        self.fit(time_series=time_series, y=y)
-        return self.generate(n_samples=n_samples)
+
+class SliceAndShuffle(BaseAugmenter):
+    """Slice the time series in k pieces and create a new time series by shuffling.
+    Args:
+        k (int): the number of slices
+        per_feature (bool): if set to True, each time series is sliced independently.
+            Otherwise, all features are sliced in the same way. Default: True
+    """
+
+    def __init__(
+        self,
+        n_segments: int,
+        per_feature: bool = True,
+    ):
+        super(SliceAndShuffle, self).__init__(per_feature)
+
+        self.n_segments = n_segments
+
+    def generate(self, n_samples: int) -> TensorLike:
+        self._check_fitted()
+        assert 0 < self.n_segments < self._data.shape[1]
+
+        seeds_idx = self._get_seeds(n_samples)
+
+        synthetic_data = []
+        labels = []
+        for i in seeds_idx:
+            sequence = self._data[i]
+            if self.per_channel:
+                raise NotImplementedError()
+            else:
+                # Randomly pick n_segments-1 points where to slice
+                idxs = np.random.randint(0, sequence.shape[0], size=self.n_segments - 1)
+                slices = []
+                start_idx = 0
+                for j in sorted(idxs):
+                    s = sequence[start_idx:j]
+                    start_idx = j
+                    slices.append(s)
+                slices.append(sequence[start_idx:])
+                np.random.shuffle(slices)
+            synthetic_data.append(sequence)
+            if self._targets is not None:
+                labels.append(self._targets[i])
+        if self._targets is not None:
+            return np.array(synthetic_data), np.array(labels)
+        else:
+            return np.array(synthetic_data)
