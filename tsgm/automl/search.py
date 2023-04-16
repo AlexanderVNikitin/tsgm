3e433c2373f6d939724cc891e48c5629de2ca75c
letiziaia <letizia852@gmail.com>
2023-04-16 21:16:09 +0300

    pass data to object

--- tsgm/automl/search.py
+++ tsgm/automl/search.py
@@ -1,22 +1,20 @@
-import itertools
 import optuna
 import typing
-import tqdm
-
-import urllib
-
-import numpy as np
-from packaging import version
-
+from tensorflow.python.types.core import TensorLike
 import tensorflow as tf
-import tensorflow_probability as tfp
-from tensorflow.keras.datasets import mnist
+
+import tqdm
 
 import logging
 
 import tsgm
 
+from tsgm.metrics.metrics import SimilarityMetric
 from tsgm.models.architectures.zoo import ConvnArchitecture
+
+# TODO: remove those 2
+import urllib
+from tensorflow.keras.datasets import mnist
 
 
 logger = logging.getLogger("automl")
@@ -24,15 +22,18 @@ logger.setLevel(logging.DEBUG)
 
 
 class ModelSelection:
-    """
-    """
+    """ """
+
     def __init__(
         self,
-        # model: tsgm.models.timeGAN.TimeGAN | tsgm.models.cvae | tsgm.models.cgan = None,
+        model: tsgm.models.timeGAN.TimeGAN
+        | tsgm.models.cvae.BetaVAE
+        | tsgm.models.cvae.cBetaVAE
+        | tsgm.models.cgan.GAN = None,
         n_trials: int = 100,
         optimize_towards: str = "maximize",
         search_space: typing.Optional[optuna.trial.Trial] = None,
-        **model_kwargs
+        **model_kwargs,
     ):
         """
         :param model:
@@ -55,21 +56,46 @@ class ModelSelection:
         #     raise NotImplementedError
 
         self.study = optuna.create_study(direction=self.direction)
+
+        # data
+        self.X_train = None
+        self.X_val = None
+        self.y_train = None
+        self.y_val = None
+
         return
+
+    def _get_dataset(self) -> typing.Tuple[tf.data.Dataset, tf.data.Dataset]:
+        N_TRAIN_EXAMPLES = 3000
+        N_VALID_EXAMPLES = 1000
+        BATCHSIZE = 128
+        if self.y_train is not None and self.y_val is not None:
+            train_ds = tf.data.Dataset.from_tensor_slices((self.X_train, self.y_train))
+            valid_ds = tf.data.Dataset.from_tensor_slices((self.X_val, self.y_val))
+        else:
+            train_ds = tf.data.Dataset.from_tensor_slices((self.X_train))
+            valid_ds = tf.data.Dataset.from_tensor_slices((self.X_val))
+
+        train_ds = train_ds.shuffle(60000).batch(BATCHSIZE).take(N_TRAIN_EXAMPLES)
+        valid_ds = valid_ds.shuffle(10000).batch(BATCHSIZE).take(N_VALID_EXAMPLES)
+        return train_ds, valid_ds
 
     def learn(self, model, optimizer, dataset, mode="eval"):
         # objective
         accuracy = tf.metrics.Accuracy("accuracy", dtype=tf.float32)
-
+        # accuracy = tf.metrics.KLDivergence(name='kullback_leibler_divergence', dtype=tf.float32)
         for batch, (images, labels) in enumerate(dataset):
             with tf.GradientTape() as tape:
                 logits = model(images, training=(mode == "train"))
                 loss_value = tf.reduce_mean(
-                    tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=labels)
+                    tf.nn.sparse_softmax_cross_entropy_with_logits(
+                        logits=logits, labels=labels
+                    )
                 )
                 if mode == "eval":
                     accuracy(
-                        tf.argmax(logits, axis=1, output_type=tf.int64), tf.cast(labels, tf.int64)
+                        tf.argmax(logits, axis=1, output_type=tf.int64),
+                        tf.cast(labels, tf.int64),
                     )
                 else:
                     grads = tape.gradient(loss_value, model.variables)
@@ -79,10 +105,10 @@ class ModelSelection:
             return accuracy
 
     def objective(self, trial):
-        # Get MNIST data.
-        train_ds, valid_ds = get_mnist()
+        # Get data
+        train_ds, valid_ds = self._get_dataset()
 
-        # Build model and optimizer.
+        # Build model and optimizer
         n_layers = trial.suggest_int("n_layers", 1, 10)
         num_hidden = trial.suggest_int("n_units", 4, 128, log=True)
         model = self.model(n_conv_blocks=n_layers, **self.model_args).model
@@ -99,10 +125,20 @@ class ModelSelection:
         # Return last validation accuracy.
         return accuracy.result()
 
-    def start(self):
+    def start(
+        self,
+        X_train: TensorLike,
+        X_val: TensorLike,
+        y_train: typing.Optional[TensorLike] = None,
+        y_val: typing.Optional[TensorLike] = None,
+    ):
         """
         Run the optimization
         """
+        self.X_train = X_train
+        self.X_val = X_val
+        self.y_train = y_train
+        self.y_val = y_val
         self.study.optimize(self.objective, n_trials=self.n_trials)
         return
 
@@ -113,10 +149,6 @@ opener = urllib.request.build_opener()
 opener.addheaders = [("User-agent", "Mozilla/5.0")]
 urllib.request.install_opener(opener)
 
-N_TRAIN_EXAMPLES = 3000
-N_VALID_EXAMPLES = 1000
-BATCHSIZE = 128
-
 
 def get_mnist():
     (x_train, y_train), (x_valid, y_valid) = mnist.load_data()
@@ -126,12 +158,7 @@ def get_mnist():
     y_train = y_train.astype("int32")
     y_valid = y_valid.astype("int32")
 
-    train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train))
-    train_ds = train_ds.shuffle(60000).batch(BATCHSIZE).take(N_TRAIN_EXAMPLES)
-
-    valid_ds = tf.data.Dataset.from_tensor_slices((x_valid, y_valid))
-    valid_ds = valid_ds.shuffle(10000).batch(BATCHSIZE).take(N_VALID_EXAMPLES)
-    return train_ds, valid_ds
+    return x_train, y_train, x_valid, y_valid
 
 
 def create_optimizer(trial):
@@ -143,19 +170,27 @@ def create_optimizer(trial):
         kwargs["learning_rate"] = trial.suggest_float(
             "rmsprop_learning_rate", 1e-5, 1e-1, log=True
         )
-        # kwargs["weight_decay"] = trial.suggest_float("rmsprop_weight_decay", 0.85, 0.99)
-        kwargs["momentum"] = trial.suggest_float("rmsprop_momentum", 1e-5, 1e-1, log=True)
+        kwargs["momentum"] = trial.suggest_float(
+            "rmsprop_momentum", 1e-5, 1e-1, log=True
+        )
     elif optimizer_selected == "Adam":
-        kwargs["learning_rate"] = trial.suggest_float("adam_learning_rate", 1e-5, 1e-1, log=True)
+        kwargs["learning_rate"] = trial.suggest_float(
+            "adam_learning_rate", 1e-5, 1e-1, log=True
+        )
     elif optimizer_selected == "SGD":
         kwargs["learning_rate"] = trial.suggest_float(
             "sgd_opt_learning_rate", 1e-5, 1e-1, log=True
         )
-        kwargs["momentum"] = trial.suggest_float("sgd_opt_momentum", 1e-5, 1e-1, log=True)
+        kwargs["momentum"] = trial.suggest_float(
+            "sgd_opt_momentum", 1e-5, 1e-1, log=True
+        )
 
     optimizer = getattr(tf.optimizers, optimizer_selected)(**kwargs)
     return optimizer
 
 
 if __name__ == "__main__":
-    ModelSelection(**dict(seq_len=28, feat_dim=28, output_dim=10)).start()
+    X_train, y_train, X_val, y_val = get_mnist()
+    ModelSelection(**dict(seq_len=28, feat_dim=28, output_dim=10)).start(
+        X_train=X_train, y_train=y_train, X_val=X_val, y_val=y_val
+    )
