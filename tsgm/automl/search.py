e61d294db309b1eb668f186ed90490029d59bbce
letiziaia <letizia852@gmail.com>
2023-04-16 22:16:23 +0300

    try using timegan and custom metric

--- tsgm/automl/search.py
+++ tsgm/automl/search.py
@@ -10,9 +10,11 @@ import logging
 import tsgm
 
 from tsgm.metrics.metrics import SimilarityMetric
-from tsgm.models.architectures.zoo import ConvnArchitecture
+from tsgm.models.timeGAN import TimeGAN
+# from tsgm.models.architectures.zoo import ConvnArchitecture
 
-# TODO: remove those 2
+# TODO: remove those when everything works
+import numpy as np
 import urllib
 from tensorflow.keras.datasets import mnist
 
@@ -49,8 +51,8 @@ class ModelSelection:
 
         self.n_trials = n_trials
         self.direction = optimize_towards
-        # self.model = model
-        self.model = ConvnArchitecture
+        self.model = model
+        # self.model = ConvnArchitecture
         self.model_args = model_kwargs
         self.search_space = search_space
         # if self.search_space is None:
@@ -150,25 +152,32 @@ class ModelSelection:
 
     def objective(self, trial):
         # Get data
-        train_ds, valid_ds = self._get_dataset()
+        # train_ds, valid_ds = self._get_dataset()
+        train_ds, valid_ds = self.X_train, self.X_val
 
         # Build model and optimizer
         self._set_search_space_params(trial)
         n_layers = getattr(self, "n_layers")
         num_hidden = getattr(self, "num_hidden")
-        model = self.model(n_conv_blocks=n_layers, **self.model_args).model
+
+        # model = self.model(n_conv_blocks=n_layers, **self.model_args).model
+        model = self.model(n_layers=n_layers)
         optimizer = self._create_optimizer(trial)
+        model.compile(optimizer)
 
         # Training and validating cycle.
         EPOCHS = 2
-        with tf.device("/cpu:0"):
-            for _ in range(EPOCHS):
-                self.learn(model, optimizer, train_ds, "train")
+        # with tf.device("/cpu:0"):
+        #     for _ in range(EPOCHS):
+        #         self.learn(model, optimizer, train_ds, "train")
 
-            objective_to_optimize = self.learn(model, optimizer, valid_ds, "eval")
-
+        #    objective_to_optimize = self.learn(model, optimizer, valid_ds, "eval")
+        model.fit(data=train_ds, epochs=EPOCHS)
+        _y = model.generate(n_samples=10)
+        objective_to_optimize = self.metric_to_optimize(_y, valid_ds)
         # Return last validation score
-        return objective_to_optimize.result()
+        # return objective_to_optimize.result()
+        return objective_to_optimize
 
     def start(
         self,
@@ -208,18 +217,72 @@ def get_mnist():
     return x_train, y_train, x_valid, y_valid
 
 
+def MinMaxScaler(data):
+  """Min Max normalizer.
+
+  Args:
+    - data: original data
+
+  Returns:
+    - norm_data: normalized data
+  """
+  numerator = data - np.min(data, 0)
+  denominator = np.max(data, 0) - np.min(data, 0)
+  norm_data = numerator / (denominator + 1e-7)
+  return norm_data
+
+
+def sine_data_generation (no, seq_len, dim):
+  """Sine data generation.
+
+  Args:
+    - no: the number of samples
+    - seq_len: sequence length of the time-series
+    - dim: feature dimensions
+
+  Returns:
+    - data: generated data
+  """
+  # Initialize the output
+  data = list()
+
+  # Generate sine data
+  for i in range(no):
+    # Initialize each time-series
+    temp = list()
+    # For each feature
+    for k in range(dim):
+      # Randomly drawn frequency and phase
+      freq = np.random.uniform(0, 0.1)
+      phase = np.random.uniform(0, 0.1)
+
+      # Generate sine signal based on the drawn frequency and phase
+      temp_data = [np.sin(freq * j + phase) for j in range(seq_len)]
+      temp.append(temp_data)
+
+    # Align row/column
+    temp = np.transpose(np.asarray(temp))
+    # Normalize to [0,1]
+    temp = (temp + 1)*0.5
+    # Stack the generated data
+    data.append(temp)
+
+  return data
+
+
 if __name__ == "__main__":
     search_space = {
         "n_layers": ("int", dict(low=1, high=10)),
         "num_hidden": ("int", dict(low=4, high=128, log=True)),
     }
-    X_train, y_train, X_val, y_val = get_mnist()
+    # X_train, y_train, X_val, y_val = get_mnist()
+    X = sine_data_generation(2000, 24, 6)
     ModelSelection(
-        search_space=search_space, **dict(seq_len=28, feat_dim=28, output_dim=10)
+        model=TimeGAN, search_space=search_space, **dict(seq_len=28, feat_dim=28, output_dim=10)
     ).start(
-        metric_to_optimize=tf.metrics.Accuracy("accuracy", dtype=tf.float32),
-        X_train=X_train,
-        y_train=y_train,
-        X_val=X_val,
-        y_val=y_val,
+        metric_to_optimize= SimilarityMetric, # tf.metrics.Accuracy("accuracy", dtype=tf.float32),
+        X_train=X,
+        X_val=X,
+        # y_train=y_train,
+        # y_val=y_val,
     )
