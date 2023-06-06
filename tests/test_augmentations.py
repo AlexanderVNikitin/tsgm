8ac787b652c2ac0c37a5d23fb336a2771a70e882
Alexander Nikitin <1243786+AlexanderVNikitin@users.noreply.github.com>
2023-06-06 22:53:32 +0300

    start refactoring

--- tests/test_augmentations.py
+++ tests/test_augmentations.py
@@ -14,28 +14,30 @@ def test_base_compose():
         compose()
 
 
-def test_gaussian_augmentation():
+def test_gaussian_augmentation_dims():
     t = np.arange(0, 10, 0.01)
     xs = np.cos(t)[None, :, None]
     n_gen = 123
     gn_aug = tsgm.models.augmentations.GaussianNoise()
-    gn_aug.fit(xs)
-    xs_gen = gn_aug.generate(n_gen)
+    xs_gen = gn_aug.generate(X=xs, n_samples=n_gen)
     assert xs_gen.shape == (n_gen, xs.shape[1], xs.shape[2])
 
-    xs_gen1 = gn_aug.generate(n_gen)
+    xs_gen1 = gn_aug.generate(X=xs, n_samples=n_gen)
     assert not np.array_equal(xs_gen, xs_gen1)
     assert xs_gen1.shape == xs_gen.shape
+
+    gn_aug = tsgm.models.augmentations.GaussianNoise(per_feature=False)
+    xs_gen = gn_aug.generate(X=xs, n_samples=n_gen)
+    assert xs_gen.shape == (n_gen, xs.shape[1], xs.shape[2])
 
 
 def test_shuffle():
     xs = np.array([[[1, 2, 3, 4], [1, 2, 3, 4]]])
     shfl_aug = tsgm.models.augmentations.Shuffle()
-    shfl_aug.fit(xs)
-    xs_gen = shfl_aug.generate(2)
+    xs_gen = shfl_aug.generate(X=xs, n_samples=2)
     assert xs_gen.shape == (2, 2, 4)
 
-    xs_gen = shfl_aug.generate(17)
+    xs_gen = shfl_aug.generate(X=xs, n_samples=17)
     assert xs_gen.shape == (17, 2, 4)
     assert all([np.allclose(x[0], x[1]) for x in xs_gen])
 
@@ -43,20 +45,18 @@ def test_shuffle():
 def test_magnitude_warping():
     xs = np.array([[[1, 2, 3, 4], [1, 2, 3, 4]]])
     magn_warp_aug = tsgm.models.augmentations.MagnitudeWarping()
-    magn_warp_aug.fit(xs)
-    xs_gen = magn_warp_aug.generate(2)
+    xs_gen = magn_warp_aug.generate(X=xs, n_samples=2)
     assert xs_gen.shape == (2, 2, 4)
 
-    xs_gen = magn_warp_aug.generate(17)
+    xs_gen = magn_warp_aug.generate(X=xs, n_samples=17)
     assert xs_gen.shape == (17, 2, 4)
 
 
 def test_window_warping():
     xs = np.array([[[1, 2, 3, 4], [1, 2, 3, 4]]])
     magn_warp_aug = tsgm.models.augmentations.WindowWarping()
-    magn_warp_aug.fit(xs)
-    xs_gen = magn_warp_aug.generate(2)
+    xs_gen = magn_warp_aug.generate(X=xs, n_samples=2)
     assert xs_gen.shape == (2, 2, 4)
 
-    xs_gen = magn_warp_aug.generate(17)
+    xs_gen = magn_warp_aug.generate(X=xs, n_samples=17)
     assert xs_gen.shape == (17, 2, 4)
