13cdfbf655816919131e0b85d9946266f8916107
letiziaia <letizia852@gmail.com>
2023-06-06 09:00:45 +0300

    download, extract and load physionet2012

--- tsgm/utils/datasets.py
+++ tsgm/utils/datasets.py
@@ -291,3 +291,93 @@ def get_gp_samples_data(num_samples: int, max_time: int,
 
     return np.random.multivariate_normal(
         mean=np.zeros(max_time), cov=sigma, size=num_samples)[:, None, :]
+
+
+def get_physionet2012() -> tuple:
+    """
+    Downloads and retrieves the Physionet 2012 dataset.
+
+    Returns:
+        tuple: A tuple containing the training, testing, and validation dataframes as
+            (train_X, train_y, test_X, test_y, val_X, val_y)
+    """
+    download_physionet2012()
+    train_X = _get_physionet_X_dataframe("physionet2012/set-a")
+    train_y = _get_physionet_y_dataframe("physionet2012/Outcomes-a.txt")
+    test_X = _get_physionet_X_dataframe("physionet2012/set-b")
+    test_y = _get_physionet_y_dataframe("physionet2012/Outcomes-b.txt")
+    val_X = _get_physionet_X_dataframe("physionet2012/set-c")
+    val_y = _get_physionet_y_dataframe("physionet2012/Outcomes-c.txt")
+    return train_X, train_y, test_X, test_y, val_X, val_y
+
+
+def download_physionet2012():
+    """
+    Downloads the Physionet 2012 dataset files from the Physionet website
+    and extracts them in local folder 'physionet2012'
+    """
+    _base_url = "https://physionet.org/files/challenge-2012/1.0.0/"
+    _destination_folder = "physionet2012"
+    X_a = "set-a.tar.gz"
+    y_a = "Outcomes-a.txt"
+
+    X_b = "set-b.zip"
+    y_b = "Outcomes-b.txt"
+
+    X_c = "set-c.tar.gz"
+    y_c = "Outcomes-c.txt"
+
+    file_utils.download(_base_url + X_a, _destination_folder)
+    file_utils.download(_base_url + y_a, _destination_folder)
+    file_utils.download(_base_url + X_b, _destination_folder)
+    file_utils.download(_base_url + y_b, _destination_folder)
+    file_utils.download(_base_url + X_c, _destination_folder)
+    file_utils.download(_base_url + y_c, _destination_folder)
+    
+    file_utils.extract_archive(_destination_folder + X_a, _destination_folder)
+    file_utils.extract_archive(_destination_folder + X_b, _destination_folder)
+    file_utils.extract_archive(_destination_folder + X_c, _destination_folder)
+
+    return
+
+
+def _get_physionet_X_dataframe(dataset_path: str) -> pd.DataFrame:
+    """
+    Reads txt files from folder 'dataset_path' and returns
+    a dataframe (X) with the Physionet dataset.
+
+    Args:
+        dataset_path (str): Path to the dataset folder.
+
+    Returns:
+        pd.DataFrame: The features (X) dataframe.
+    """
+    txt_all = list()
+    for f in os.listdir(dataset_path):
+        with open(os.path.join(dataset_path, f), 'r') as fp:
+            txt = fp.readlines()
+
+        # add recordid as a column
+        recordid = txt[1].rstrip('\n').split(',')[-1]
+        txt = [t.rstrip('\n').split(',') + [int(recordid)] for t in txt]
+        txt_all.extend(txt[1:])
+    df = pd.DataFrame(txt_all, columns=['time', 'parameter', 'value', 'recordid'])
+    return df
+
+
+def _get_physionet_y_dataframe(file_path: str) -> pd.DataFrame:
+    """
+    Reads txt files from folder 'dataset_path' and returns
+    a dataframe (y) with the Physionet data.
+
+    Args:
+        dataset_path (str): Path to the dataset folder.
+
+    Returns:
+        pd.DataFrame: The target (y) dataframe.
+    """
+    y = pd.read_csv(file_path)
+    y.set_index('RecordID', inplace=True)
+    y.index.name = 'recordid'
+    y.reset_index(inplace=True)
+    return y
